30660eac4ba85efaea97ab1bbd78304e69e2585c
Eric Windisch <eric@iopipe.com>
2016-08-02 19:27:13 +0000

    _ not . in debug console.log
    Signed-off-by: Eric Windisch <eric@iopipe.com>

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ function _make_generateLog(emitter, func, start_time, config, context) {
         }
 
     if (config.debug) {
-      console.log("IOPIPE-DEBUG: ", response.body)
+      console.log("IOPIPE-DEBUG: ", response_body)
       callback()
       return
     }
