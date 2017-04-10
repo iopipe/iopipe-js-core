5038ec155368000acfbf119b96091f4602160f83
Erica Windisch <e@windisch.us>
2017-04-10 14:39:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var Promise = require('bluebird')
 var request = require('request')
 var os = require('os')
 var https = require('https')
+var net = require('net')
 
 var system = (process.platform === 'linux') ? require('./src/system.js') : require('./src/mockSystem.js')
 var getCollectorUrl = require('./src/collector.js')
