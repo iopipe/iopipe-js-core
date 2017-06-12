3a3584092b7f7ba7fd756155dab04c8ffd4c8ad4
Pam Selle <pamela.selle@gmail.com>
2017-06-12 12:15:31 -0400

    Fix tests to test src

---
+++ spec/config_spec.js
@@ -0,0 +1,36 @@
+const setConfig = require('../src/config.js')
+
+describe('setting up config object', function() {
+  it('can accept 0 arguments and returns default config', function() {
+    expect(setConfig()).toEqual({
+      host: 'metrics-api.iopipe.com',
+      path: '/v0/event',
+      clientId: '',
+      debug: false,
+      networkTimeout: 5000,
+      captureTimeouts: true,
+      timeoutWindow: 50,
+      installMethod: 'manual'
+    })
+  })
+
+  it('configures a client id', function() {
+    expect(setConfig({token:'foo'}).clientId).toEqual('foo')
+    expect(setConfig({clientId: 'bar'}).clientId).toEqual('bar')
+  })
+
+  it('sets preferences for order of client id config', function() {
+    delete process.env.IOPIPE_TOKEN
+    delete process.env.IOPIPE_CLIENTID
+
+    // takes token over clientId
+    expect(setConfig({clientId: 'bar', token: 'foo'}).clientId).toEqual('foo')
+
+    process.env['IOPIPE_CLIENTID'] = 'qux'
+    expect(setConfig().clientId).toEqual('qux')
+
+    // takes IOPIPE_TOKEN over IOPIPE_CLIENTID
+    process.env['IOPIPE_TOKEN'] = 'baz'
+    expect(setConfig().clientId).toEqual('baz')
+  })
+})
