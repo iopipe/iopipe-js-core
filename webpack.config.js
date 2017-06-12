365f546b403356a6880955296d2210bd2c5e9dcf
Pam Selle <pamela.selle@gmail.com>
2017-06-12 15:21:06 -0400

    Rename dist to lib

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   target: "node",
   entry: "./index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'lib'),
     filename: "./iopipe.js",
     libraryTarget: 'commonjs2',
     library: "iopipe"
