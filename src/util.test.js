746c0b3483d266c3d5a48b0d30d20a1cf9f45a3b
Pam Selle <pamela.selle@gmail.com>
2018-03-15 16:39:00 -0400

    Add coverage for other data types

--- src/util.test.js
+++ src/util.test.js
@@ -6,4 +6,8 @@ test('converts to string', () => {
   expect(convertToString(NaN)).toBe('NaN');
   expect(convertToString({ foo: 1 })).toBe('{"foo":1}');
   expect(convertToString([1, 2])).toBe('[1,2]');
+  expect(convertToString(true)).toBe('true');
+  expect(convertToString(100)).toBe('100');
+  expect(convertToString(null)).toBe('null');
+  expect(convertToString(Symbol('foo'))).toBe('Symbol(foo)');
 });
