ba7352c9235c8c5b6e0a9462b091046db449d8a7
Corey Light <coreylight@gmail.com>
2018-07-12 13:18:42 -0500

    Move supported regions outside of function block

--- src/fileUploadMeta/getSigner.js
+++ src/fileUploadMeta/getSigner.js
@@ -1,10 +1,11 @@
 import { SUPPORTED_REGIONS } from '../constants';
 
+const supported = new Map(SUPPORTED_REGIONS);
+// allow us-east-1 for signer util
+supported.set('us-east-1', true);
+
 export default function getSignerHostname() {
   const { AWS_REGION } = process.env;
-  const supported = new Map(SUPPORTED_REGIONS);
-  // allow us-east-1 for signer util
-  supported.set('us-east-1', true);
   return `https://signer.${
     supported.has(AWS_REGION) ? AWS_REGION : 'us-west-2'
   }.iopipe.com/`;
