1515eca1ee69d11e876375381a9b0be7a2ec3b42
Stephanie Gawroriski <xer@multiphasicapps.net>
2019-03-01 14:36:20 -0500

    Add parse of IOPIPE_COLLECTOR_URL to set the collector URL.

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,5 +1,7 @@
 import CosmiConfig from './cosmi';
 
+const url = require('url');
+
 export default class EnvironmentConfig extends CosmiConfig {
   /**
    * Environment variable configuration
@@ -30,6 +32,12 @@ export default class EnvironmentConfig extends CosmiConfig {
       : super.enabled;
   }
 
+  get host() {
+    return process.env.IOPIPE_COLLECTOR_URL
+      ? url.parse(process.env.IOPIPE_COLLECTOR_URL).hostname
+      : super.host;
+  }
+
   get installMethod() {
     return process.env.IOPIPE_INSTALL_METHOD || super.installMethod;
   }
@@ -38,6 +46,12 @@ export default class EnvironmentConfig extends CosmiConfig {
     return Number.isInteger(parseInt(process.env.IOPIPE_NETWORK_TIMEOUT, 10))
       ? parseInt(process.env.IOPIPE_NETWORK_TIMEOUT, 10)
       : super.networkTimeout;
+  }
+
+  get path() {
+    return process.env.IOPIPE_COLLECTOR_URL
+      ? url.parse(process.env.IOPIPE_COLLECTOR_URL).pathname
+      : super.path;
   }
 
   get timeoutWindow() {
