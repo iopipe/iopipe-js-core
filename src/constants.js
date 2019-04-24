b894f4a69ca62a865d9a2382f97e79ce814149be
Michael Lavers <kolanos@gmail.com>
2019-04-24 09:15:06 -0700

    fix: Add New AWS Regions
    Closes #321

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,25 @@
 const SUPPORTED_REGIONS = new Map(
   [
     'ap-northeast-1',
+    'ap-northeast-2',
+    'ap-south-1',
+    'ap-southeast-1',
+    'ap-southeast-2',
+    'ca-central-1',
+    'eu-central-1',
+    'eu-west-1',
+    'eu-west-2',
+    'us-east-2',
+    'us-west-1',
+    'us-west-2'
+    // construct the map where each key is a region and the value is true (supported)
+  ].map(s => [s, true])
+);
+
+// we also support us-east-1 but due to DNS constraints we cannot list it here
+const SUPPORTED_SIGNER_REGIONS = new Map(
+  [
+    'ap-northeast-1',
     'ap-southeast-2',
     'eu-west-1',
     'us-east-2',
@@ -12,4 +31,4 @@ const SUPPORTED_REGIONS = new Map(
   ].map(s => [s, true])
 );
 
-export { SUPPORTED_REGIONS };
+export { SUPPORTED_REGIONS, SUPPORTED_SIGNER_REGIONS };
